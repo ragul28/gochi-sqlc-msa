065a4b27cd904d817db745cb2e167ab5a7c50876
ragul28 <ragulh28@gmail.com>
2023-11-18 20:18:29 +0530

    Add configstore to the get env config

--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -2,6 +2,21 @@ package utils
 
 import "os"
 
+// ConfigStore struct stores the flags
+type ConfigStore struct {
+	DB_URL string
+	PORT   string
+}
+
+func ConfigEnv() ConfigStore {
+	var cfg ConfigStore
+
+	cfg.DB_URL = GetEnv("DB_URL", "postgresql://postgres:postgres@127.0.0.1:5432/postgres?sslmode=disable")
+	cfg.PORT = GetEnv("PORT", "8080")
+
+	return cfg
+}
+
 func GetEnv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
