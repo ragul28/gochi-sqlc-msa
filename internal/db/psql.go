a21688103194a997fe5f74c82543b0de2845046e
ragul28 <ragulh28@gmail.com>
2023-11-19 19:21:49 +0530

    Add DB creation conn for psql

---
+++ internal/db/psql.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func CreateConnection(dbUrl string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
