824456301a733b477a590719951ab5d609cd5ac4
ragul28 <ragulh28@gmail.com>
2023-11-19 19:54:23 +0530

    Add chi router with http server on main

--- cmd/usersrv/main.go
+++ cmd/usersrv/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/ragul28/gochi-sqlc-msa/internal/db"
 	"github.com/ragul28/gochi-sqlc-msa/pkg/logger"
 	"github.com/ragul28/gochi-sqlc-msa/pkg/utils"
@@ -18,5 +21,15 @@ func main() {
 	if err != nil {
 		slog.Error("Error connection to DB", "err", err, slog.String("package", "main"))
 		return
+	}
+	dbconn.Ping()
+
+	router := chi.NewRouter()
+	// queries := sqlc.New(dbconn)
+
+	slog.Info(fmt.Sprintf("Server started on Port: %s", cfg.PORT))
+	err = http.ListenAndServe(":"+cfg.PORT, router)
+	if err != nil {
+		slog.Error("Error http server", err, slog.String("package", "main"))
 	}
 }
