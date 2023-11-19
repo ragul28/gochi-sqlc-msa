d16af79eb60704a781b629d6f5c20d851da23cee
ragul28 <ragulh28@gmail.com>
2023-11-19 20:03:21 +0530

    Update user handler on main router

--- cmd/usersrv/main.go
+++ cmd/usersrv/main.go
@@ -7,6 +7,11 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/ragul28/gochi-sqlc-msa/internal/db"
+	"github.com/ragul28/gochi-sqlc-msa/internal/db/sqlc"
+	userhandler "github.com/ragul28/gochi-sqlc-msa/internal/user/handler"
+	userrepository "github.com/ragul28/gochi-sqlc-msa/internal/user/repository"
+	routes "github.com/ragul28/gochi-sqlc-msa/internal/user/router"
+	userservice "github.com/ragul28/gochi-sqlc-msa/internal/user/service"
 	"github.com/ragul28/gochi-sqlc-msa/pkg/logger"
 	"github.com/ragul28/gochi-sqlc-msa/pkg/utils"
 )
@@ -22,10 +27,15 @@ func main() {
 		slog.Error("Error connection to DB", "err", err, slog.String("package", "main"))
 		return
 	}
-	dbconn.Ping()
 
 	router := chi.NewRouter()
-	// queries := sqlc.New(dbconn)
+	queries := sqlc.New(dbconn)
+
+	userRepo := userrepository.NewUserRepository(dbconn, queries)
+	userService := userservice.NewUserService(userRepo)
+	userHandler := userhandler.NewUserHandler(userService)
+
+	routes.InitUserRoutes(router, userHandler)
 
 	slog.Info(fmt.Sprintf("Server started on Port: %s", cfg.PORT))
 	err = http.ListenAndServe(":"+cfg.PORT, router)
