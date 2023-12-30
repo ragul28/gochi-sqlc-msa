e735ab038afd19af60b33e5434ccca165463e5de
ragul28 <ragulh28@gmail.com>
2023-12-30 19:56:44 +0530

    Add entity struct

---
+++ internal/user/entity/entity.go
@@ -0,0 +1,25 @@
+package entity
+
+import "time"
+
+type UserEntity struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password,omitempty"`
+	Address   string    `json:"address,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserResponse struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ManyUsersResponse struct {
+	Users []UserResponse `json:"users"`
+}
